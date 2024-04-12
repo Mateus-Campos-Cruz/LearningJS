854f3ae696fb05879fc0ee7c360173b813a94ecc
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-12 13:45:54 -0300

    AprendendoJS

---
+++ 2.ComoExcutarJavaScript.js
@@ -0,0 +1,11 @@
+//Ferramentas para executar JS
+
+//https://jsfiddle.net/
+
+//F12 ou Inspecionar - Pelo Console do Navegar - 
+//É possível usar todas as bibliotecas de um site carregado 
+//Use o biblioteca de Query do https://jquery.com/
+
+//Terminal do Node.js
+
+//Extensão do Visual Code - Code Runner
