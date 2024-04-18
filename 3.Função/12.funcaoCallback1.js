91350790605f08332133faaac9f4177f847e5397
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-18 11:32:41 -0300

    Função Callback

---
+++ 3.Função/12.funcaoCallback1.js
@@ -0,0 +1,7 @@
+const fabricante = ["Mercedes", "Audi", "BMW", ]
+
+function imprimir(nome, indice){
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricante.forEach(imprimir) // Para cada parametro que encontrar vai chamar a função de volta
