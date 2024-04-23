6c6e48556de13a676e8ec434edf04b21093b6fa9
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-23 10:39:06 -0300

    resposta 27

---
+++ 3.Função/lista_exercicios_resposta_4/Resposta-Mateus/resposta27.js
@@ -0,0 +1,4 @@
+/**27)​ Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas 
+crianças e calcule se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e em 
+quantos anos isso acontecerá. Utilize centímetros para as unidades de medida. */
+
