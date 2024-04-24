657b3bae1ccb0f1d5d8ed888719699938a3cfa72
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-24 13:24:28 -0300

    react 4

---
+++ React/exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,8 @@
+import React, {Fragment} from 'react'
+
+export default props => 
+    <Fragment>
+    <h1>Bom dia {props.nome}!</h1>
+        <h2>Até breve</h2>
+    </Fragment>
+        
--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
-ReactDOM.render(<Primeiro />, document.getElementById('root'))
+ReactDOM.render(<BomDia nome="Mateus" />, document.getElementById('root'))
