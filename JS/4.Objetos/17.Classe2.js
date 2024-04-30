3859a1c70eee480ed3e3dad891423be105283c06
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-30 08:02:14 -0300

    Classe

---
+++ JS/4.Objetos/17.Classe2.js
@@ -0,0 +1,23 @@
+//Herança em Classe
+
+class Avo {
+    constructor(sobrenome){
+        this.sobrenome = sobrenome
+    }
+}
+
+    class Pai extends Avo {
+        constructor(sobrenome, profissao = 'Professor'){
+            super(sobrenome) // Invoca o atributo da superclasse
+            this.profissao = profissao
+        }
+}
+
+class Filho extends Pai {
+    constructor(){
+        super('Silva')
+    }
+}
+
+const filho = new Filho
+console.log(filho)
