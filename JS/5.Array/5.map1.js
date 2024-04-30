b23a8a9dafed8e9b2247f150bf7907e57f523342
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-30 13:54:47 -0300

    map1

---
+++ JS/5.Array/5.map1.js
@@ -0,0 +1,12 @@
+/**Função map mapeia um array para passar as informações para
+ * outro array, com os dados transformados, o array novo vai ter
+ * o mesmo tamanho do array fonte.
+ * 
+For com propósito 
+*/
+
+const nums = [1, 2, 3, 4, 5]
+
+let resultado = nums. map(function(e){
+    
+})
