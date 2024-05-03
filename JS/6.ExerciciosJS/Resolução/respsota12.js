786af2600cc3c90b0828442c26540dd5b250d6e4
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-05-03 13:36:55 -0300

    Resposta 12

---
+++ JS/6.ExerciciosJS/Resolução/respsota12.js
@@ -0,0 +1,14 @@
+/**
+ * Quando temos um objeto e manipulamos seus atributos, adicionando, atualizando ou removendo-os, 
+ * estamos apenas modificando-o, mas, em essência, o objeto continua o mesmo, ou seja, a sua 
+ * referência de memória é a mesmo.
+
+Num projeto que você esta trabalhando, você foi designado a refatorar diversas 
+funções para que façam cópias de objetos e manipulem os dados dessas cópias, 
+com intuito de evitar efeitos indesejáveis em algumas situações devido a referências 
+a objetos. Abaixo, está a descrição de umas dessas funções.
+
+Você escreverá uma função que recebe um objeto como primeiro param, e, como segundo param, 
+o nome de uma propriedade contida nesse objeto. Em seguida, retorne uma cópia desse objeto sem 
+a propriedade especificada no segundo param.
+ */
