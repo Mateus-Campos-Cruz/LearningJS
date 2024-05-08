96156a673decb91bfe949d7be790bab07875a055
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-05-08 13:04:46 -0300

    Conceitos Web

---
+++ ConceitosWeb/introducaoWeb.js
@@ -0,0 +1,42 @@
+/**##História da Internet - Marcos
+ * 
+ * 1950s - Computador Eletrônico
+ * 1960s - ARPANET - Departamento de Defesa Americano
+ * 1970s - TCP/IP - Robert E, Kahn & Vint Cerf
+ * 1980s - WWW - Tim Berners-Lee
+ * 1990s - Internet no Brasil
+ * 2000s - iG, Redes Sociais, Internet em casa
+ * 
+ * ##Topologias de redes - Rede de computadores
+ * 
+ * Centralizada - Vários computadores ligados a um nó central (Switch)
+ * Distribuida - Vários computadores terá várias rotas
+ * 
+ * ##Protocolos 
+ * 
+ * TCP/IP - Transmission Control Protocol / Internet Protocol
+ * Orientado a conexão e confiável
+ * 
+ * ##Camadas TCP/IP - Padrão Layers
+ * 
+ * Aplicação - HTTP, FTP, SMTP, POP, Comunicação Processo-A-Processo, Npumero da porta
+ * Transporte - TCP(Confiável), UDP(Melhor esforço - DNS), Comunicação Host-a-Host, Confiabilidade, integridade
+ * Internet - IP, Conexão entre redes, Transferência de pacotes
+ * Rede/Física - Wifi, Ethernet, MAC address - Física (Hardware) e Enlace (Software)
+ * 
+ * ##IPv4 - Decimais - 32 bits - 4 octetos (Binários) 
+ * Exemplo: 192.168.1.12 - 11000000.10101000.0000001.00001100
+ * 
+ * Classe A - Primeiro octeto entre 0 e 127 - 16.77.216 endereços de IP cada rede
+ * 192.168.100.230 - (192) Rede (168.100.230) Host
+ * Poucas redes e muitas máquinas - Não disponibilizado para 
+ * 
+ * Máscara de Subrede - Número associado ao IP (255.0.0.0)
+ * 
+ * Classe B - Primeiro octeto entre 128 e 191 - 65.536 endereços IP cada rede
+ * Classe C - Primeiro octeto entre 192 e 223 - 256 endereços IP cada rede
+ * 
+ * - Classes reservadas
+ * Classe D - Primeiro octeto entre 224 e 239 
+ * CLasse E - Primeiro octeto entre 240 e 255
+ */
