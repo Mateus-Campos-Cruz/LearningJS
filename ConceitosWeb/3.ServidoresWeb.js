fb0acb42e39f1b6a7c96d0a983f4eeae0e213853
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-05-09 10:36:47 -0300

    Servidores Web

---
+++ ConceitosWeb/3.ServidoresWeb.js
@@ -0,0 +1,26 @@
+/**
+ * Servidores Web
+ * 
+ * Apache, Tomcat, MicrosoftIIS, Node, JBoss, NGNX
+ * 
+ * ##Virtual hosting
+ * 
+ * Virtual hosting é uma técnica que permite hospedar múltiplos sites em um único servidor físico. 
+ * Isso é alcançado através da configuração de software, como servidores web, para responder a 
+ * solicitações de diferentes domínios e direcionar cada solicitação para o conteúdo correto, 
+ * com base no domínio solicitado.
+ * 
+ * ##Estrutura de Pastas
+ * 
+ * Web Server - Pastas com Imagens, CSS, JS, Index.html, about.html
+ * 
+ * ##Web Stack
+ * 
+ * Uma Web Stack, também conhecida como pilha de tecnologias web, é um conjunto de tecnologias 
+ * e ferramentas utilizadas para desenvolver e executar aplicações web. Essas tecnologias trabalham 
+ * em conjunto para fornecer a infraestrutura e o ambiente necessários para que uma aplicação web 
+ * funcione de forma eficiente.
+ * 
+ * 
+ * 
+ */
