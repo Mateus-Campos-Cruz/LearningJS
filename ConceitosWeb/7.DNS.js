62a829f523a57f40ad12cd4ede2ba1745c21f66a
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-05-10 13:05:36 -0300

    HTTPS

---
+++ ConceitosWeb/7.DNS.js
@@ -0,0 +1,70 @@
+/**
+ * DNS - Domain Name System
+ * 
+ * Traduzir nomes de domínio em endereço IP
+ * 
+ * www.cod3r.com.br -> 54.69.61.89
+ * 
+ * Pode configurar o seu DNS direto no provedor de hosting
+ * 
+ * DNS funciona sobre o protocolo UDP na porta 53
+ * 
+ * ##Funcionamento Básico do DNS
+ * 
+ * 1. Usuário informa o endereço no brownser
+ * 2. Computador envia uma consulta DNS para o servidor local
+ * 3. O servidor responde com o endereço IP
+ * 4. Computador acessa o servidor a partir do IP obtido
+ * 
+ * Busca recursiva
+ * Busca iterativa
+ * 
+ * ##DNS Records
+ * 
+ * A - IPv4
+ * AAAA - IPv6
+ * MX - Servidor de Email
+ * NS - Servidor DNS
+ * CNAME - Canonical Name (Apelido)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
---
+++ ConceitosWeb/8.HTTPS(Seguro).js
@@ -0,0 +1,65 @@
+/**
+ * ##HTTPS (Security)
+ * 
+ * Dados criptografados usando os protocolos TLS ou SSL
+ * Protege contra interceptação (Man-In-The-Middle)
+ * Criptografa todas as informações: URL, cookies e headers
+ * Usa certificados digitais
+ * 
+ * ##Autoridade Certificadoras
+ * Emitem e validam certificados para habilitar os protocolos HTTPS
+ * 
+ * HTTP + TLS (SSLv4) - TLS - Transport Layer Security Protocol
+ * HTTP + SSL - SSL - Secure Sockets Layers Protocol
+ * 
+ * Certificado <> Protocolo - 
+ * "Certificado SLL/TLS" - Errado
+ * "Certificado usado com SSL/TLS" - Certo
+ * 
+ * Sites para gerar certificados gratuitos
+ * 
+ * Let's Encrypt
+ * Certbot
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+
+
+
+ */
